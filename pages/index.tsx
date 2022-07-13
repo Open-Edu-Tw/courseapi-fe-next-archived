3c062a04663a90080bb5157f644b12b3d91b9881
pan93412 <pan93412@gmail.com>
2022-07-13 20:19:12 +0800

    refactor: use tsconfig's “path” as alias

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next";
 import { tw } from 'twind'
-import { DownloadIcon } from "../components/icons";
+import { Button } from "@/components";
+import { DownloadIcon } from "@/icons";
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1 className={tw`font-bold text-center`}>Hello Next.js</h1>
       <DownloadIcon />
+      <Button size="medium" color="accent" label="登入" />
     </div>
   );
 };

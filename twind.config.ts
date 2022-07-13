b8384a26deac63eb72aff3e067074ae993f65930
pan93412 <pan93412@gmail.com>
2022-07-13 19:33:50 +0800

    init: configuration

---
+++ twind.config.ts
@@ -0,0 +1,13 @@
+import { Configuration } from "twind";
+import * as colors from "twind/colors";
+
+const config: Configuration = {
+  theme: {
+    colors,
+    borderRadius: {
+      DEFAULT: "0.5rem",
+    },
+  },
+};
+
+export default config;
